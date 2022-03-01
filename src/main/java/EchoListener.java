0cc0ccea5c1846b5aa8c613b982c0429655de7c5
Ilya Tampio <53406289+Quakumei@users.noreply.github.com>
2022-03-01 17:07:10 +0300

    dev: lavaplayer import + template for !play

--- src/main/java/EchoListener.java
+++ src/main/java/EchoListener.java
@@ -1,17 +1,19 @@
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.requests.restaction.MessageAction;
-import net.dv8tion.jda.api.utils.AttachmentOption;
+import net.dv8tion.jda.api.managers.AudioManager;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.net.URL;
 import java.util.Locale;
+import java.util.Objects;
 
 public class EchoListener extends ListenerAdapter {
     @Override
@@ -20,16 +22,17 @@ public class EchoListener extends ListenerAdapter {
         // We don't want to respond to other bot accounts, including ourself
         Message message = event.getMessage();
         String content = message.getContentRaw();
+        TextChannel tc = event.getTextChannel();
+        Guild guild = event.getGuild();
+        Member member = event.getMember();
+
         // getContentRaw() is an atomic getter
         // getContentDisplay() is a lazy getter which modifies the content for e.g. console view (strip discord formatting)
         if (content.equals("!ping")) {
-            MessageChannel channel = event.getChannel();
-            channel.sendMessage("Pong!").queue(); // Important to call .queue() on the RestAction returned by sendMessage(...)
-        }
-        if (content.equals("!info") ) {
-            TextChannel tc = event.getTextChannel();
-
-
+            tc.sendMessage("Pong!").queue(); // Important to call .queue() on the RestAction returned by sendMessage(...)
+        } else if (content.split(" ")[0].equals("!echo")) {
+            tc.sendMessage(message).queue();
+        } else if (content.equals("!info")) {
             EmbedBuilder EB = new EmbedBuilder();
             EB.setThumbnail("https://github.com/Quakumei/PagluechBot/raw/main/src/main/resources/pagluech.png");
             EB.setTitle("PagluechBot", "https://github.com/Quakumei/PagluechBot/");
@@ -43,23 +46,15 @@ public class EchoListener extends ListenerAdapter {
 //                File f = new File("target/classes/pagluech.png");
 //                tc.sendFile(f, "pagluech.png").queue();
 //            }
-        }
-        if (content.equals("!join")) {
-            Guild guild = event.getGuild();
-            Member member = event.getMember();
-//            VoiceChannel myChannel = guild.getVoiceChannelById();
-        }
-        
-        if (content.split(" ")[0].equals("!show")) {
-            TextChannel tc = event.getTextChannel();
-            try{
+        } else if (content.split(" ")[0].equals("!show")) {
+            try {
                 URL url = new URL(content.split(" ")[1]);
                 BufferedImage img = ImageIO.read(url);
                 String imgFormat = url.toString().replaceAll(".*\\.", "").toLowerCase(Locale.ROOT);
-                if (!imgFormat.equals("jpg") && !imgFormat.equals("png")){
-                    throw new Exception("wrong format (got: "+imgFormat+")");
+                if (!imgFormat.equals("jpg") && !imgFormat.equals("png")) {
+                    throw new Exception("wrong format (got: " + imgFormat + ")");
                 }
-                File file = new File("temp."+imgFormat);
+                File file = new File("temp." + imgFormat);
                 ImageIO.write(img, imgFormat, file);
                 tc.sendFile(file).append("Success! :)").queue();
 
@@ -67,6 +62,31 @@ public class EchoListener extends ListenerAdapter {
             } catch (Exception e) {
 
                 tc.sendMessage("Error while fetching image: ").append(e.toString()).queue();
+            }
+        } else if (content.split(" ")[0].equals("!play")) {
+            try {
+                URL url = new URL(content.split(" ")[1]);
+                // TODO: fix regex
+                String source = url.toString().replaceAll(".*\\.", "").toLowerCase(Locale.ROOT);
+                if (!source.equals("youtube")) {
+                    throw new Exception("wrong format (got: " + source + ")");
+                }
+                // TODO: Voice channel magic
+                assert member != null;
+                VoiceChannel myChannel = (VoiceChannel) member.getVoiceState().getChannel();
+                AudioManager audioManager = guild.getAudioManager();
+
+
+                //TODO: MAKE IT WORK
+//                AudioPlayerManager pM = new DefaultAudioPlayerManager();
+//                AudioSourceManagers.registerRemoteSources(pM);
+//                AudioPlayer ap = pM.createPlayer();
+//
+//                audioManager.setSendingHandler(new AudioPlayerSendHandler(ap));
+//                audioManager.openAudioConnection(myChannel);
+
+            } catch (Exception e) {
+                tc.sendMessage("Error while starting audiostream: ").append(e.toString()).queue();
             }
 
 
